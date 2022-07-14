fdd49f2ca72d28ddebe17d1153974e00056c60c7
Toya <66713857+NorauDn@users.noreply.github.com>
2022-07-14 15:17:19 +0900

    きゃらくたーていすうのていぎ

--- yoketoru_vs22/Form1.cs
+++ yoketoru_vs22/Form1.cs
@@ -15,6 +15,18 @@ namespace yoketoru_vs22
     {
         const bool isDebug = true;
 
+        const int PlayerMax = 1;
+        const int EnemyMax = 10;
+        const int ItemMax = 10;
+        const int ChrMax = PlayerMax + EnemyMax + ItemMax;
+        Label[] chrs = new Label[ChrMax];
+        const int PlayerIndex = 0;
+        const int EnemyIndex = PlayerIndex + PlayerMax;//（PlayerMax = 1）+（ PlayerIndex = 0）＝１
+        const int ItemIndex = EnemyIndex + EnemyMax;//（EnemyIndex =（PlayerMax = 1）+（ PlayerIndex = 0）＝１）+（EnemyMax = 10）＝11
+
+        const int PlayerText = "(・ω・)";
+
+
         enum State
         {//何も宣言しない場合は上から0.1...となる。が、わからないので－1をもうける。-1なのは初期化のためにも、自然に入らんものってことかな？
 
